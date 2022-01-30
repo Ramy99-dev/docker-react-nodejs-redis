9e135e8800859ec1ba8fab45819ee099453b5dc8
Ramy99-dev <63595300+Ramy99-dev@users.noreply.github.com>
2022-01-30 14:32:20 +0100

    create docker-compose file

--- server/index.js
+++ server/index.js
@@ -1,24 +1,50 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const Todo = require('./models/todo')
 
 const app = express()
-import client  from './redisDB.js'
-app.post('/',async (res,req)=>{
-    await client.lPush("todo",JSON.stringify(req.body.todo))
-    res.json({"msg":"Added Successfully"})
-})
+mongoose.connect('mongodb://mongo:27017/',(res)=>{
+     console.log("Connected to Database")
+   
+    
+  })
 
 
-
-app.get('/',async (req ,res)=>{
-    let data = await client.lRange(0,-1)
-    console.log(data)
-    res.json({"data":data})
-})
 
 app.use(cors())
 app.use(express.json())
 
+
+app.post('/', (req,res)=>{
+    let todo = new Todo(req.body.todo)
+    todo.save()
+    .then((result)=>{
+       console.log("Saved Successfully")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
+
+
+
+app.get('/', (req ,res)=>{
+    Todo.find()
+    .then((result)=>{
+        res.send(result)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+    
+})
+
+app.delete('/', (req ,res)=>{
+    
+    
+})
+
 app.listen(7082,()=>{
-    console.log("Listening to port 7082")
+    console.log("Listening to port 3082")
 })
--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
+import { useEffect,useState } from "react";
 
 
 function App() {
+  let data = null;
+  let input = null;
+  let [todos,setTodos] = useState([])
+ 
+  useEffect(async () => {
+    data = await fetch("http://localhost:7082/");
+    
+    let todo = await data.json()
+    console.log(todo)
+    setTodos(todo.data)
+
+  }, [])
+  const addTodo = async () => {
+    if(input)
+    {
+      data = await fetch("http://localhost:7082/", {
+      method: 'POST',
+      headers: {
+
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ todo: input })
+    });
+    setTodos([...todos , input])
+    console.log(data)
+    }
+    
+  }
+
+  const removeTodo = async (todo,pos) => {
+    
+    data = await fetch(`http://localhost:7082/`, {
+      method: 'DELETE',
+      headers: {
+
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ todo })
+    });
+    let todosCopy = [...todos]
+    todosCopy.splice(pos,1)
+    setTodos([...todosCopy])
+    //console.log(data)
+  }
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen bg-slate-900 ">
       <div className="flex flex-row">
-        <input type="text" className="border-solid border-2 border-sky-500 font-Inconsolata focus:outline-none" />
-        <button className="bg-cyan-600 px-3 py-2.1 ml-3 hover:bg-cyan-900 font-Inconsolata text-white">Add</button>
+        <input type="text" onChange={(e) => {
+          if(e.target.value)
+          {
+            input = e.target.value
+          }
+        }} className="border-solid border-2 border-sky-500 font-Inconsolata focus:outline-none" />
+        <button onClick={addTodo} className="bg-cyan-600 px-3 py-2.1 ml-3 hover:bg-cyan-900 font-Inconsolata text-white">Add</button>
       </div>
       <div className="grid gap-20 grid-cols-2 mt-3">
-        <span className="text-white font-Inconsolata">Coding</span>
-        <div>
-           <button className="bg-green-600 mr-5">👍</button>
-           <button className="bg-red-400">❌</button>
-        </div>
+        {todos?.map((todo,i) => {
+         return ( <>
+            <span  className="text-white font-Inconsolata">{todo.replace(/\"/g,"")}</span>
+            <div >
+              
+              <button onClick={()=>removeTodo(todo,i)} className="bg-red-400">❌</button>
+            </div></>)
+        })}
+
       </div>
     </div>
   );
